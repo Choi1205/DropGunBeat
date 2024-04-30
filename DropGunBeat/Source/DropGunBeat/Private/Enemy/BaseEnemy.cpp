58c8d4518abd9feba8e08ad6d6b77e8422ec4b94
Choi1205 <chlghksfhr@naver.com>
2024-04-30 19:23:20 +0900

    [Update] 0430 최환록
    적 기본형 코드 수정 및 추가

--- DropGunBeat/Source/DropGunBeat/Private/Enemy/BaseEnemy.cpp
+++ DropGunBeat/Source/DropGunBeat/Private/Enemy/BaseEnemy.cpp
@@ -2,12 +2,30 @@
 
 
 #include "Enemy/BaseEnemy.h"
+#include "Components/CapsuleComponent.h"
+#include "Components/SkeletalMeshComponent.h"
+#include "Components/StaticMeshComponent.h"
 
 // Sets default values
 ABaseEnemy::ABaseEnemy()
 {
  	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
+
+	GetMesh()->SetRelativeLocation(FVector(0.0f, 0.0f, -88.0f));
+	GetMesh()->SetRelativeRotation(FRotator(0.0f, 0.0f, -90.0f));
+
+	vestMeshComp = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Vest"));
+	vestMeshComp->SetupAttachment(GetMesh());
+
+	helmetMeshComp = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Helmet"));
+	helmetMeshComp->SetupAttachment(GetMesh());
+
+	gunMeshComp = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Gun"));
+	gunMeshComp->SetupAttachment(GetMesh());
+
+	firePoint = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("FirePoint"));
+	firePoint->SetupAttachment(gunMeshComp);
 
 }
 
@@ -16,12 +34,34 @@ void ABaseEnemy::BeginPlay()
 {
 	Super::BeginPlay();
 	
+	currentHP = maxHP;
 }
 
 // Called every frame
 void ABaseEnemy::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
+
+	switch (enemyState)
+	{
+	case EPhaseState::IDLE:
+		Idle(DeltaTime);
+		break;
+	case EPhaseState::MOVE:
+		move(DeltaTime);
+		break;
+	case EPhaseState::AIM:
+		Aim(DeltaTime);
+		break;
+	case EPhaseState::SHOOT:
+		Shoot();
+		break;
+	case EPhaseState::DIE:
+		Die();
+		break;
+	default:
+		break;
+	}
 
 }
 
@@ -30,5 +70,48 @@ void ABaseEnemy::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent
 {
 	Super::SetupPlayerInputComponent(PlayerInputComponent);
 
+}
+
+void ABaseEnemy::Idle(float deltaTime)
+{
+	currentTimer = FMath::Min(currentTimer + deltaTime, idleCooldown);
+	if(currentTimer == idleCooldown){
+		currentTimer = 0.0f;
+		SetActorRotation((targetPlace - GetActorLocation()).Rotation());
+		enemyState = EPhaseState::MOVE;
+	}
+}
+
+void ABaseEnemy::move(float deltaTime)
+{
+	if(FVector::Distance(GetActorLocation(), targetPlace) < 34.0f) {
+		enemyState = EPhaseState::AIM;
+	}
+	else {
+		FVector toward = targetPlace - GetActorLocation();
+		toward.Normalize();
+		AddMovementInput(toward, speed);
+	}
+}
+
+void ABaseEnemy::Aim(float deltaTime)
+{
+	currentTimer = FMath::Min(currentTimer + deltaTime, fireCooldown);
+	if (currentTimer == fireCooldown) {
+		currentTimer = 0.0f;
+		enemyState = EPhaseState::SHOOT;
+	}
+	else {
+		//플레이어를 조준하는 명령어를 넣기
+	}
+}
+
+void ABaseEnemy::Shoot()
+{
+	
+}
+
+void ABaseEnemy::Die()
+{
 }
 
--- DropGunBeat/Source/DropGunBeat/Public/Enemy/BaseEnemy.h
+++ DropGunBeat/Source/DropGunBeat/Public/Enemy/BaseEnemy.h
@@ -6,6 +6,15 @@
 #include "GameFramework/Character.h"
 #include "BaseEnemy.generated.h"
 
+UENUM(BlueprintType)
+enum class EPhaseState : uint8 {
+	IDLE,
+	MOVE,
+	AIM,
+	SHOOT,
+	DIE
+};
+
 UCLASS()
 class DROPGUNBEAT_API ABaseEnemy : public ACharacter
 {
@@ -14,6 +23,51 @@ class DROPGUNBEAT_API ABaseEnemy : public ACharacter
 public:
 	// Sets default values for this character's properties
 	ABaseEnemy();
+
+	//////////////
+	// 컴포넌트 //
+	//////////////
+	
+	UPROPERTY(EditDefaultsOnly)
+	class UStaticMeshComponent* vestMeshComp;
+
+	UPROPERTY(EditDefaultsOnly)
+	class UStaticMeshComponent* helmetMeshComp;
+
+	UPROPERTY(EditDefaultsOnly)
+	class UStaticMeshComponent* gunMeshComp;
+
+	UPROPERTY(EditDefaultsOnly)
+	class USceneComponent* firePoint;
+
+private:
+
+	//////////
+	// 변수 //
+	//////////
+
+	UPROPERTY(VisibleAnywhere, Category = "EnemyStat")
+	EPhaseState enemyState;
+	UPROPERTY(EditDefaultsOnly, Category = "EnemyStat|HP")
+	int32 maxHP;
+	UPROPERTY(VisibleAnywhere, Category = "EnemyStat|HP")
+	int32 currentHP = 1;
+	UPROPERTY(EditAnywhere, Category = "EnemyStat|Attack")
+	int32 numToFire;
+	UPROPERTY(VisibleAnywhere, Category = "EnemyStat|Attack")
+	int32 fireCounter;
+	UPROPERTY(EditAnywhere, Category = "EnemyStat|Attack")
+	float fireCooldown = 3.0f;
+	UPROPERTY(EditAnywhere, Category = "EnemyStat")
+	float idleCooldown = 1.0f;
+	UPROPERTY(VisibleAnywhere, Category = "EnemyStat")
+	float currentTimer = 0.0f;
+	UPROPERTY(EditAnywhere, Category = "EnemyStat|Move")
+	FVector targetPlace;
+	UPROPERTY(EditAnywhere, Category = "EnemyStat|Move")
+	float speed = 1.0f;
+
+	class AGunPlayer* playerREF;
 
 protected:
 	// Called when the game starts or when spawned
@@ -25,5 +79,15 @@ public:
 
 	// Called to bind functionality to input
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+	//////////
+	// 함수 //
+	//////////
+	
+	void Idle(float deltaTime);
+	void move(float deltaTime);
+	void Aim(float deltaTime);
+	void Shoot();
+	void Die();
 
 };
