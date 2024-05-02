c931a036861eb1230bfb2cb3b2fef2405d632fde
jjj12g <159858734+jjj12g@users.noreply.github.com>
2024-05-03 00:06:54 +0900

    일단 버그패치

--- DropGunBeat/Source/DropGunBeat/Private/PlayerWidget.cpp
+++ DropGunBeat/Source/DropGunBeat/Private/PlayerWidget.cpp
@@ -1,16 +1,16 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
+//
+//#include "PlayerWidget.h"
+//#include <../../../../../../../Source/Runtime/UMG/Public/Components/TextBlock.h>
+//#include "GunPlayer.h"
 
-#include "PlayerWidget.h"
-#include <../../../../../../../Source/Runtime/UMG/Public/Components/TextBlock.h>
-#include "GunPlayer.h"
-
-
-void UPlayerWidget::SetCurrentBullet()
-{
-	if (CurrentBullet)
-	{
-		CurrentBullet->SetVisibility(ESlateVisibility::Visible);
-	}
-
-}
+//
+//void UPlayerWidget::SetCurrentBullet()
+//{
+//	if (CurrentBullet)
+//	{
+//		CurrentBullet->SetVisibility(ESlateVisibility::Visible);
+//	}
+//
+//}
